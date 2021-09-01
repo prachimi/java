7b71521aa3a299c1ed2b9b8e9517329b67e6caa3
prachi mishra <prachimishra0707@gmail.com>
2021-09-01 21:28:08 +0530

    new java file

---
+++ src/box.java
@@ -0,0 +1,15 @@
+public class box {
+    int x;
+    public static void main(String r[])
+    {
+        box ob=new box();
+        ob.x=4;
+        System.out.println(ob.x);
+        box ob2=new box();
+        ob2.x=7;
+        System.out.println(ob2.x);
+
+
+
+    }
+}
